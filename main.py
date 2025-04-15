15dd076ec3fa49a19b86bc25d485367f5c91587d
Dan <gvozdevdd@gmail.com>
2025-04-15 15:07:37 +0400

    format

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
-from stats import *
+
+from stats import count_characters, count_words, print_sorted_characters
+
 
 def get_book_text(filepath: str) -> str:
     content: str
@@ -8,20 +10,22 @@ def get_book_text(filepath: str) -> str:
 
     return content
 
+
 def main():
     if len(sys.argv) != 2:
-        print('Usage: python3 main.py <path_to_book>')
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    
+
     text = get_book_text(sys.argv[1])
 
-    print('BOOKBOT'.center(50, "="))
-    print('Analyzing book found at', sys.argv[1])
-    print('Word Count'.center(50, "="))
-    print('Found', count_words(text), "total words")
-    print('Character Count'.center(50, "="))
+    print("BOOKBOT".center(50, "="))
+    print("Analyzing book found at", sys.argv[1])
+    print("Word Count".center(50, "="))
+    print("Found", count_words(text), "total words")
+    print("Character Count".center(50, "="))
     print_sorted_characters(count_characters(text))
-    print('END'.center(50, "="))
+    print("END".center(50, "="))
+
 
 if __name__ == "__main__":
-     main()
+    main()
--- stats.py
+++ stats.py
@@ -1,13 +1,16 @@
 from collections import Counter
 
+
 def count_words(content: str) -> int:
     return len(str.split(content))
+
 
 def count_characters(content: str) -> dict[str, int]:
     return Counter(c for c in content.lower() if c.isalpha())
 
+
 def print_sorted_characters(dict_chars: dict[str, int]):
     for key, value in sorted(dict_chars.items(), key=lambda x: x[1], reverse=True):
         print(f"{key}: {value}")
-    
+
     pass
