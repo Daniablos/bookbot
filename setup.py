91ff565debcf432b8f33820f1259f855f3972fc1
Dan <gvozdevdd@gmail.com>
2025-04-18 09:33:32 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bookbot",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Daniablos",
+    description="A simple bookbot for reading and processing text",
+    url="https://github.com/Daniablos/bookbot",
+)
